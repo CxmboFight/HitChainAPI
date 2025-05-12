ded77b442ec93c962c2b7afc471c13a2c038bc14
CxmboFight <cxmbofight.business@gmail.com>
2025-05-12 14:28:02 +0100

    Update ConfigManager.java

--- config/ConfigManager.java
+++ config/ConfigManager.java
@@ -1,1 +1,25 @@
+package com.hitchainapi.config;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ConfigManager {
+
+    private final AdvancedConfigLoader configLoader;
+
+    public ConfigManager(JavaPlugin plugin) {
+        this.configLoader = new AdvancedConfigLoader(plugin, "config.yml");
+    }
+
+    public AdvancedConfigLoader getConfigLoader() {
+        return configLoader;
+    }
+
+    public void loadDefaults() {
+        configLoader.set("someSetting", "defaultValue");
+        configLoader.set("enableFeatureX", true);
+    }
+
+    public void reload() {
+        configLoader.reloadConfig();
+    }
+}
