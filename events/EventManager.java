4f97381edcd69b017c01e150ae313df02f04df33
CxmboFight <cxmbofight.business@gmail.com>
2025-05-12 14:34:16 +0100

    Update EventManager.java

--- events/EventManager.java
+++ events/EventManager.java
@@ -1,1 +1,22 @@
+package com.hitchainapi.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import com.hitchainapi.HitChainPlugin;
+
+public class EventManager {
+
+    private final HitChainPlugin plugin;
+
+    public EventManager(HitChainPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void registerEvents() {
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        pluginManager.registerEvents(new BlockListener(), plugin);
+        pluginManager.registerEvents(new EntityListener(), plugin);
+        pluginManager.registerEvents(new PlayerListener(), plugin);
+    }
+}
 
