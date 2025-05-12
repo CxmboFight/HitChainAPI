1e44487ac090df60d8893df8bbf4c481a58c6342
CxmboFight <cxmbofight.business@gmail.com>
2025-05-12 14:33:40 +0100

    Update BlockListener.java

--- events/BlockListener.java
+++ events/BlockListener.java
@@ -1,1 +1,21 @@
+package com.hitchainapi.events;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class BlockListener implements Listener {
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        // Custom logic for block break event
+        event.getPlayer().sendMessage("You broke a block: " + event.getBlock().getType());
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        // Custom logic for block place event
+        event.getPlayer().sendMessage("You placed a block: " + event.getBlock().getType());
+    }
+}
