72df565985352a013587b4e6091c965c23bfa761
CxmboFight <cxmbofight.business@gmail.com>
2025-05-12 14:34:41 +0100

    Update PlayerListener.java

--- events/PlayerListener.java
+++ events/PlayerListener.java
@@ -1,1 +1,21 @@
+package com.hitchainapi.events;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerListener implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // Custom logic for player join event
+        event.getPlayer().sendMessage("Welcome to the server, " + event.getPlayer().getName());
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        // Custom logic for player quit event
+        event.getPlayer().sendMessage("Goodbye, " + event.getPlayer().getName());
+    }
+}
